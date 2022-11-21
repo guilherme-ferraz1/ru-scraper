cd547ede56077c42864581c878e07a6eaab3edc7
Guilherme Ferraz <gferraz@visiblemagic.com>
2022-11-21 14:54:35 -0300

    release: 2.0.0

--- index.js
+++ index.js
@@ -58,24 +58,24 @@ const convert = (html) => {
   }
 
 const formatData = (data) => { 
-  //  const date = data[7][0].split(`\n`)[1]
+   const date = data[6][0].split(`\n`)[1]
     
-    // const dia = date.substring(0, 2)
-    // const mes = mesesDic[date.substring(3, 6)]
-    // const ano =  date.substring(7, 9)
+    const dia = date.substring(0, 2)
+    const mes = mesesDic[date.substring(3, 6)]
+    const ano =  date.substring(7, 9)
 
-    // let dataFinal = undefined
-    // if (dia && mes && ano ) dataFinal = `${dia}/${mes}/${ano}`
+    let dataFinal = undefined
+    if (dia && mes && ano ) dataFinal = `${dia}/${mes}/${ano}`
 
-    // const menu = [
-    //   createItem(data[1]),
-    //   createItem(data[2]),
-    //   createItem(data[3]),
-    //   createItem(data[4]),
-    //   createItem(data[5]),
-    //   createItem(data[6]),
-    //   createItem(data[7]),
-    // ]
+    const menu = [
+      createItem(data[0]),
+      createItem(data[1]),
+      createItem(data[2]),
+      createItem(data[3]),
+      createItem(data[4]),
+      createItem(data[5]),
+      createItem(data[6]),
+    ]
 
     const fixas = ["Arroz Parbolizado", "Arroz integral", "Feijão"]
 
@@ -139,109 +139,142 @@ const formatData = (data) => {
     ]
 
     return {
-      "dataFinal": '27/11/2022',
-      "cardapio": MENU_TEMPORARY
+      "dataFinal": dataFinal,
+      "cardapio": menu
     }
 }
 
-const getCarnes = (string) => {
-  let arr = []
-  let newStr = string.toUpperCase()
+// const getCarnes = (string) => {
+//   let arr = []
+//   let newStr = string.toUpperCase()
 
-  if (string.toUpperCase().includes('CARNE JANTAR')) {
-    arr.push(string.substring(newStr.indexOf('CARNE JANTAR') + 13, newStr.indexOf(('COMPLEMENTO'))))
-    arr.push(string.substring(newStr.indexOf('CARNE ALMOÇO') + 13, newStr.indexOf(('CARNE JANTAR'))))
-    return arr
-  }
+//   if (string.toUpperCase().includes('CARNE JANTAR')) {
+//     arr.push(string.substring(newStr.indexOf('CARNE JANTAR') + 13, newStr.indexOf(('COMPLEMENTO'))))
+//     arr.push(string.substring(newStr.indexOf('CARNE ALMOÇO') + 13, newStr.indexOf(('CARNE JANTAR'))))
+//     return arr
+//   }
 
-  if (string.toUpperCase().includes('CARNE (JANTAR)')) {
-    arr.push(string.substring(newStr.indexOf('CARNE (JANTAR)') + 15, newStr.indexOf('COMPLEMENTO')))
-    arr.push(string.substring(newStr.indexOf('CARNE (ALMOÇO)') + 15, newStr.indexOf(('CARNE (ALMOÇO)'))))
-    return arr
-  }
+//   if (string.toUpperCase().includes('CARNE (JANTAR)')) {
+//     arr.push(string.substring(newStr.indexOf('CARNE (JANTAR)') + 15, newStr.indexOf('COMPLEMENTO')))
+//     arr.push(string.substring(newStr.indexOf('CARNE (ALMOÇO)') + 15, newStr.indexOf(('CARNE (ALMOÇO)'))))
+//     return arr
+//   }
 
-  arr.push(string.substring(newStr.indexOf('CARNE') + 6, newStr.indexOf(('COMPLEMENTO'))))
-  return arr
-}
+//   arr.push(string.substring(newStr.indexOf('CARNE') + 6, newStr.indexOf(('COMPLEMENTO'))))
+//   return arr
+// }
 
-const getComplementos = (string) => {
-  let arr = []
-  let newStr = string.toUpperCase()
+// const getComplementos = (string) => {
+//   let arr = []
+//   let newStr = string.toUpperCase()
 
-  if (string.toUpperCase().includes('COMPLEMENTO JANTAR')) {
-    arr.push(string.substring(newStr.indexOf('COMPLEMENTO JANTAR') + 19, newStr.indexOf(('SALADA'))))
-    arr.push(string.substring(newStr.indexOf('COMPLEMENTO ALMOÇO') + 19, newStr.indexOf(('COMPLEMENTO JANTAR'))))
-    return arr
-  }
+//   if (string.toUpperCase().includes('COMPLEMENTO JANTAR')) {
+//     arr.push(string.substring(newStr.indexOf('COMPLEMENTO JANTAR') + 19, newStr.indexOf(('SALADA'))))
+//     arr.push(string.substring(newStr.indexOf('COMPLEMENTO ALMOÇO') + 19, newStr.indexOf(('COMPLEMENTO JANTAR'))))
+//     return arr
+//   }
 
-  if (string.toUpperCase().includes('COMPLEMENTO (JANTAR)')) {
-    arr.push(string.substring(newStr.indexOf('COMPLEMENTO (JANTAR)') + 21, newStr.indexOf('SALADA')))
-    arr.push(string.substring(newStr.indexOf('COMPLEMENTO (ALMOÇO)') + 21, newStr.indexOf(('COMPLEMENTO (ALMOÇO)'))))
-    return arr
-  }
+//   if (string.toUpperCase().includes('COMPLEMENTO (JANTAR)')) {
+//     arr.push(string.substring(newStr.indexOf('COMPLEMENTO (JANTAR)') + 21, newStr.indexOf('SALADA')))
+//     arr.push(string.substring(newStr.indexOf('COMPLEMENTO (ALMOÇO)') + 21, newStr.indexOf(('COMPLEMENTO (ALMOÇO)'))))
+//     return arr
+//   }
 
-  arr.push(string.substring(newStr.indexOf('COMPLEMENTO') + 12, newStr.indexOf(('SALADA'))))
-  return arr
-}
+//   arr.push(string.substring(newStr.indexOf('COMPLEMENTO') + 12, newStr.indexOf(('SALADA'))))
+//   return arr
+// }
 
-const getSalada = (string) => {
-  let arr = []
-  let newStr = string.toUpperCase()
+// const getSalada = (string) => {
+//   let arr = []
+//   let newStr = string.toUpperCase()
 
-  if (string.toUpperCase().includes('SALADA 2')) {
-    arr.push(string.substring(newStr.indexOf('SALADA 1') + 9, newStr.indexOf(('SALADA 2'))))
-    if (newStr.includes(('SOBREMESA'))) {
-      arr.push(string.substring(newStr.indexOf('SALADA 2') + 9, newStr.indexOf(('SOBREMESA'))))
-    } else {
-      arr.push(string.substring(newStr.indexOf('SALADA 2') + 9, newStr.indexOf(('MOLHO'))))
-    }
-    return arr
-  }
+//   if (string.toUpperCase().includes('SALADA 2')) {
+//     arr.push(string.substring(newStr.indexOf('SALADA 1') + 9, newStr.indexOf(('SALADA 2'))))
+//     if (newStr.includes(('SOBREMESA'))) {
+//       arr.push(string.substring(newStr.indexOf('SALADA 2') + 9, newStr.indexOf(('SOBREMESA'))))
+//     } else {
+//       arr.push(string.substring(newStr.indexOf('SALADA 2') + 9, newStr.indexOf(('MOLHO'))))
+//     }
+//     return arr
+//   }
 
-  arr.push(string.substring(newStr.indexOf('SALADA 1') + 9, newStr.indexOf(('SOBREMESA'))))
-  return arr
-}
+//   arr.push(string.substring(newStr.indexOf('SALADA 1') + 9, newStr.indexOf(('SOBREMESA'))))
+//   return arr
+// }
 
-const getSobremesa = (string) => {
-  let arr = []
-  let newStr = string.toUpperCase()
+// const getSobremesa = (string) => {
+//   let arr = []
+//   let newStr = string.toUpperCase()
 
-  if (string.toUpperCase().includes('SOBREMESA')) {
-    arr.push(string.substring(newStr.indexOf('SOBREMESA') + 10, newStr.indexOf(('MOLHO SALADA'))))
-  }
+//   if (string.toUpperCase().includes('SOBREMESA')) {
+//     arr.push(string.substring(newStr.indexOf('SOBREMESA') + 10, newStr.indexOf(('MOLHO SALADA'))))
+//   }
 
-  return arr
-}
+//   return arr
+// }
 
-const getMolho = (string) => {
-  let arr = []
-  let newStr = string.toUpperCase()
+// const getMolho = (string) => {
+//   let arr = []
+//   let newStr = string.toUpperCase()
 
-  if (string.toUpperCase().includes('MOLHO SALADA')) {
-    arr.push(string.substring(newStr.indexOf('MOLHO SALADA') + 13))
-  }
+//   if (string.toUpperCase().includes('MOLHO SALADA')) {
+//     arr.push(string.substring(newStr.indexOf('MOLHO SALADA') + 13))
+//   }
 
-  return arr
-}
+//   return arr
+// }
 
 const createItem = (menu) => {
   const e = menu[1]
   const item = e.split('\n')
-  const arr = item
-    .map((i) => i.split(/\s+/))
-  delete arr[1]
-  const bigArr = [].concat(...arr)
-  const cleaned = bigArr.filter((item) => item !== undefined)
-  const replaced = cleaned.map((i) => i.replace(/[!@#$%:^&*]/g, ""))
-  const bigString = replaced.join(' ')
-  
+  item.shift()
   let itemObj = {}
-  itemObj.carne = getCarnes(bigString)
+  itemObj.carne = []
   itemObj.fixas = ["Arroz Parbolizado", "Arroz integral", "Feijão"]
-  itemObj.complemento = getComplementos(bigString)
-  itemObj.salada = getSalada(bigString)
-  itemObj.molho = getMolho(bigString)
-  itemObj.sobremesa = getSobremesa(bigString)
+  itemObj.complemento = []
+  itemObj.salada = []
+  itemObj.molho = []
+  itemObj.sobremesa = []
+  let n = 0
+  while (n < item.length) {
+    if (n == item.length - 1) {
+      const trimmed = item[n].substring(item[n].indexOf(':') + 2)
+      item[n].includes(':') ? itemObj.sobremesa.push(trimmed) : itemObj.sobremesa.push(item[n])
+    }
+    if (n == item.length - 2) {
+      const trimmed = item[n].substring(item[n].indexOf(':') + 2)
+      item[n].includes(':') ? itemObj.molho.push(trimmed) : itemObj.molho.push(item[n])
+    }
+    if (item[n].toUpperCase().includes('CARNE:')) {
+      const trimmed = item[n].substring(item[n].indexOf(':') + 2)
+      itemObj.carne.push(trimmed)
+    }
+    if (item[n].toUpperCase().includes('CARNE (JANTAR)')) {
+      const trimmed = item[n].substring(item[n].indexOf(':') + 2)
+      itemObj.carne.push(trimmed + ' (jantar)')
+    }
+    if (item[n].toUpperCase().includes('COMPLEMENTO:')) {
+      const trimmed = item[n].substring(item[n].indexOf(':') + 2)
+      itemObj.complemento.push(trimmed)
+    }
+    if (item[n].toUpperCase().includes('COMPLEMENTO (JANTAR)')) {
+      const trimmed = item[n].substring(item[n].indexOf(':') + 2)
+      itemObj.complemento.push(trimmed + ' (jantar)')
+    }
+    if (item[n].toUpperCase().includes('SALADAS')) {
+      const trimmed = item[n].substring(item[n].indexOf(':') + 2)
+      itemObj.salada.push(trimmed)
+    }
+    if (item[n].toUpperCase().includes('SOBREMESA')) {
+      const trimmed = item[n].substring(item[n].indexOf(':') + 2)
+      itemObj.sobremesa.push(trimmed)
+    }
+    if (item[n].toUpperCase().includes('MOLHO:')) {
+      const trimmed = item[n].substring(item[n].indexOf(':') + 2)
+      itemObj.molho.push(trimmed)
+    }
+    n++
+  }
   return itemObj
 }
 
